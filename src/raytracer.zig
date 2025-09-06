36c3ed634c1dd63c3e76d80baf86fe19da6e6af4
Shivan <shivantheman@gmail.com>
2025-09-06 11:28:53 +0200

    fix pointer bugs

--- src/raytracer.zig
+++ src/raytracer.zig
@@ -2,18 +2,18 @@ const std = @import("std");
 
 // Vectors
 const Vec4 = struct {
-    x: f32,
-    y: f32,
-    z: f32,
-    w: f32,
+    x: f64,
+    y: f64,
+    z: f64,
+    w: f64,
 };
 
 const Vec3 = struct {
-    x: f32,
-    y: f32,
-    z: f32,
+    x: f64,
+    y: f64,
+    z: f64,
 
-    fn add(self: *Vec3, other: *Vec3) Vec3 {
+    fn add(self: Vec3, other: Vec3) Vec3 {
         return Vec3{
             .x = self.x + other.x,
             .y = self.y + other.y,
@@ -21,7 +21,7 @@ const Vec3 = struct {
         };
     }
 
-    fn sub(self: *Vec3, other: *Vec3) Vec3 {
+    fn sub(self: Vec3, other: Vec3) Vec3 {
         return Vec3{
             .x = self.x - other.x,
             .y = self.y - other.y,
@@ -29,7 +29,7 @@ const Vec3 = struct {
         };
     }
 
-    fn scalarMul(self: *Vec3, scalar: f32) Vec3 {
+    fn scalarMul(self: Vec3, scalar: f64) Vec3 {
         return Vec3{
             .x = self.x * scalar,
             .y = self.y * scalar,
@@ -37,7 +37,7 @@ const Vec3 = struct {
         };
     }
 
-    fn cross(self: *Vec3, other: *Vec3) Vec3 {
+    fn cross(self: Vec3, other: Vec3) Vec3 {
         return Vec3{
             .x = self.y * other.z - self.z * other.y,
             .y = self.z * other.x - self.x * other.z,
@@ -45,19 +45,27 @@ const Vec3 = struct {
         };
     }
 
-    fn dot(self: *Vec3, other: *Vec3) f32 {
+    fn dot(self: Vec3, other: Vec3) f64 {
         return self.x * other.x + self.y * other.y + self.z * other.z;
     }
 
-    fn length(self: *Vec3) f32 {
+    fn length(self: Vec3) f64 {
         return std.math.sqrt(self.x * self.x + self.y * self.y + self.z * self.z);
     }
 
-    fn normalize(self: *Vec3) Vec3 {
-        return self.div(self.length());
+    fn normalize(self: Vec3) Vec3 {
+        const len = self.magnitude();
+        if (len > 0.0) {
+            return self.scalarMul(1.0 / len);
+        }
+        return Vec3{
+            .x = self.x,
+            .y = self.y,
+            .z = self.z,
+        }; // the return type is not a ptr, so we must return a copy
     }
 
-    fn div(self: *Vec3, other: *Vec3) Vec3 {
+    fn div(self: Vec3, other: Vec3) Vec3 {
         return Vec3{
             .x = self.x / other.x,
             .y = self.y / other.y,
@@ -65,7 +73,7 @@ const Vec3 = struct {
         };
     }
 
-    fn negate(self: *Vec3) Vec3 {
+    fn negate(self: Vec3) Vec3 {
         return Vec3{
             .x = -self.x,
             .y = -self.y,
@@ -73,7 +81,7 @@ const Vec3 = struct {
         };
     }
 
-    fn magnitude(self: *Vec3) f64 {
+    fn magnitude(self: Vec3) f64 {
         return std.math.sqrt(self.x * self.x + self.y * self.y + self.z * self.z);
     }
 };
@@ -108,7 +116,7 @@ const Sphere = struct {
     radius: f64,
     mat: Material,
 
-    fn ray_intersect(self: *Sphere, origin: Vec3, direction: Vec3, t: f64) IntersectResult {
+    fn ray_intersect(self: Sphere, origin: Vec3, direction: Vec3, t: *f64) IntersectResult {
         // origin of ray to sphere
 
         const L = self.center.sub(origin);
@@ -125,24 +133,24 @@ const Sphere = struct {
         const r2 = self.radius * self.radius;
 
         if (d2 > r2) {
-            return IntersectResult{ .intersects = false, .dist = t };
+            return IntersectResult{ .intersects = false, .dist = t.* };
         }
 
         const mag = std.math.sqrt(r2 - d2);
 
         // intersection points
-        t = TC - mag;
+        t.* = TC - mag;
         const t1 = TC + mag;
 
-        if (t < 0.0) {
-            t = t1;
+        if (t.* < 0.0) {
+            t.* = t1;
         }
 
-        if (t < 0.0) {
-            return IntersectResult{ .intersects = false, .dist = t };
+        if (t.* < 0.0) {
+            return IntersectResult{ .intersects = false, .dist = t.* };
         }
 
-        return IntersectResult{ .intersects = true, .dist = t };
+        return IntersectResult{ .intersects = true, .dist = t.* };
     }
 };
 
@@ -191,58 +199,52 @@ pub fn main() !void {
     const allocator = gpa.allocator();
 
     // Spheres
-    var spheres: std.ArrayList(Sphere) = .empty;
+    var spheres: [4]Sphere = undefined;
 
-    defer spheres.deinit(allocator);
-
-    try spheres.append(allocator, Sphere{
+    spheres[0] = Sphere{
         .center = Vec3{ .x = -3, .y = 0, .z = -16 },
         .radius = 2,
         .mat = ivory,
-    });
+    };
 
-    try spheres.append(allocator, Sphere{
+    spheres[1] = Sphere{
         .center = Vec3{ .x = -1.0, .y = -1.5, .z = -12 },
         .radius = 2,
         .mat = glass,
-    });
+    };
 
-    try spheres.append(allocator, Sphere{
+    spheres[2] = Sphere{
         .center = Vec3{ .x = 1.5, .y = -0.5, .z = -18 },
         .radius = 3,
         .mat = redRubber,
-    });
+    };
 
-    try spheres.append(allocator, Sphere{
+    spheres[3] = Sphere{
         .center = Vec3{ .x = 7, .y = 5, .z = -18 },
         .radius = 4,
         .mat = mirror,
-    });
+    };
 
     // Lights
-    var lights: std.ArrayList(Light) = .empty;
-    defer lights.deinit(allocator);
+    var lights: [3]Light = undefined;
 
-    try lights.append(allocator, Light{
+    lights[0] = Light{
         .position = Vec3{ .x = -20, .y = 20, .z = 20 },
         .intensity = 4.5,
-    });
+    };
 
-    try lights.append(allocator, Light{
+    lights[1] = Light{
         .position = Vec3{ .x = 20, .y = -20, .z = -20 },
         .intensity = 1.8,
-    });
+    };
 
-    try lights.append(allocator, Light{
+    lights[2] = Light{
         .position = Vec3{ .x = 30, .y = 20, .z = 30 },
         .intensity = 1.7,
-    });
+    };
 
     // framebuffer with size width * height
-    var framebuffer: std.ArrayList(Vec3) = .empty;
-    defer framebuffer.deinit(allocator);
-    try framebuffer.resize(allocator, width * height);
-
+    var framebuffer: [width * height]Vec3 = undefined; // stack allocated
     const origin = Vec3{ .x = 0, .y = 0, .z = 0 };
 
     std.debug.print("Rendering scene...\n", .{});
@@ -250,17 +252,18 @@ pub fn main() !void {
     for (0..height) |y| {
         for (0..width) |x| {
             const xFloat: f64 = @floatFromInt(x);
-            var xPos: f64 = xFloat + 0.5 - width / 2;
+            const xPos: f64 = xFloat + 0.5 - width / 2;
             const yFloat: f64 = @floatFromInt(y); // type inference can't work across all the disaprate ops in the calc
-            var yPos = yFloat + 0.5 - height / 2;
-            var zPos = -height / (2 * std.math.tan(fov / 2));
+            const yPos = yFloat + 0.5 - height / 2;
+            const zPos = -@as(f64, height) / (2.0 * std.math.tan((@as(f64, fov) / 2.0)));
 
             const rayDirection = Vec3{ .x = xPos, .y = yPos, .z = zPos };
-            const rayDirectionNormalized = rayDirection.normalize(); // is this fine?
-            // framebuffer.append(allocator, CastRay)
-            // const color = trace(ray, spheres, lights, allocator);
+            const rayDirectionNormalized = rayDirection.normalize();
+            framebuffer[y + x * width] = castRay(origin, rayDirectionNormalized, &spheres, &lights, 0, allocator);
         }
     }
+
+    std.debug.print("Framebuffer filled\n", .{});
 }
 
 fn castRay(origin: Vec3, direction: Vec3, spheres: []Sphere, lights: []Light, depth: u32, allocator: std.mem.Allocator) Vec3 {
@@ -268,7 +271,7 @@ fn castRay(origin: Vec3, direction: Vec3, spheres: []Sphere, lights: []Light, de
     var N = Vec3{ .x = 0, .y = 0, .z = 0 };
     var mat = Material{ .diffuseColor = Vec3{ .x = 0, .y = 0, .z = 0 }, .abledo = Vec4{ .x = 0, .y = 0, .z = 0, .w = 0 }, .specularExponent = 0, .refractiveIndex = 0 };
 
-    const intersects = sceneIntersect(origin, direction, point, N, mat, spheres, allocator);
+    const intersects = sceneIntersect(origin, direction, &point, &N, &mat, spheres);
 
     if (intersects or depth > 4) {
         return BG_COLOR;
@@ -277,7 +280,7 @@ fn castRay(origin: Vec3, direction: Vec3, spheres: []Sphere, lights: []Light, de
     const p = 1E-3;
 
     const reflect_direction = reflect(direction, N);
-    var reflect_origin: Vec3 = null;
+    var reflect_origin: Vec3 = Vec3{ .x = 0, .y = 0, .z = 0 };
 
     if (reflect_direction.dot(N) < 0) {
         reflect_origin = point.sub(N.scalarMul(p));
@@ -289,7 +292,7 @@ fn castRay(origin: Vec3, direction: Vec3, spheres: []Sphere, lights: []Light, de
 
     const refract_direction = refract(direction, N, mat.refractiveIndex, 1.0);
 
-    var refactor_origin: Vec3 = null;
+    var refactor_origin: Vec3 = Vec3{ .x = 0, .y = 0, .z = 0 };
 
     if (refract_direction.dot(N) < 0) {
         refactor_origin = point.sub(N.scalarMul(p));
@@ -301,12 +304,12 @@ fn castRay(origin: Vec3, direction: Vec3, spheres: []Sphere, lights: []Light, de
 
     // shadows + diffuse
 
-    var diffuse_light_intensity = 0.0;
-    var specular_light_intensity = 0.0;
+    var diffuse_light_intensity: f64 = 0.0;
+    var specular_light_intensity: f64 = 0.0;
 
-    var light_distance = 0.0;
+    var light_distance: f64 = 0.0;
 
-    var shadow_origin: Vec3 = null;
+    var shadow_origin: Vec3 = Vec3{ .x = 0, .y = 0, .z = 0 };
 
     for (lights) |light| {
         const light_direction = light.position.sub(point).normalize();
@@ -319,22 +322,22 @@ fn castRay(origin: Vec3, direction: Vec3, spheres: []Sphere, lights: []Light, de
         }
 
         var shadow_point: Vec3 = Vec3{ .x = 0, .y = 0, .z = 0 };
-        const shadow_normal: Vec3 = Vec3{ .x = 0, .y = 0, .z = 0 };
-        const temp_mat = Material{ .diffuseColor = Vec3{ .x = 0, .y = 0, .z = 0 }, .abledo = Vec4{ .x = 0, .y = 0, .z = 0, .w = 0 }, .specularExponent = 0, .refractiveIndex = 0 };
+        var shadow_normal: Vec3 = Vec3{ .x = 0, .y = 0, .z = 0 };
+        var temp_mat: Material = Material{ .diffuseColor = Vec3{ .x = 0, .y = 0, .z = 0 }, .abledo = Vec4{ .x = 0, .y = 0, .z = 0, .w = 0 }, .specularExponent = 0, .refractiveIndex = 0 };
 
-        const shadow_intersects = sceneIntersect(shadow_origin, light_direction, shadow_point, shadow_normal, temp_mat, spheres, allocator);
+        const shadow_intersects = sceneIntersect(shadow_origin, light_direction, &shadow_point, &shadow_normal, &temp_mat, spheres);
         if (shadow_intersects and shadow_point.sub(shadow_origin).magnitude() < light_distance) {
             continue;
         }
 
         diffuse_light_intensity += light.intensity * @max(0.0, light_direction.dot(N));
 
-        specular_light_intensity += std.math.pow(@max(0.0, reflect(light_direction, N).dot(direction)), mat.specularExponent) * light.intensity;
+        specular_light_intensity += std.math.pow(f64, @max(0.0, reflect(light_direction, N).dot(direction)), mat.specularExponent) * light.intensity;
     }
 
     // big math
     const lit = mat.diffuseColor.scalarMul(diffuse_light_intensity).scalarMul(mat.abledo.x);
-    const sp = specular_light_intensity & mat.abledo.y;
+    const sp = specular_light_intensity * mat.abledo.y;
     const specularVec = Vec3{
         .x = 1.0,
         .y = 1.0,
@@ -351,34 +354,34 @@ fn castRay(origin: Vec3, direction: Vec3, spheres: []Sphere, lights: []Light, de
 }
 
 // by casting a ray we can see which object is intersected with it
-fn sceneIntersect(origin: Vec3, direction: Vec3, hit: Vec3, N: Vec3, mat: Material, spheres: []Sphere, allocator: std.mem.Allocator) bool {
-    var max_sphere_distance = std.math.floatMax(f64);
+fn sceneIntersect(origin: Vec3, direction: Vec3, hit: *Vec3, N: *Vec3, mat: *Material, spheres: []Sphere) bool {
+    var max_sphere_distance: f64 = std.math.floatMax(f64);
 
     for (spheres) |sphere| {
-        const distI: f64 = 0; // FIXME: Does this need be passed as *distI?
-        const intersect = sphere.ray_intersect(origin, direction, distI);
+        var distI: f64 = 0; // FIXME: Does this need be passed as *distI?
+        const intersect = sphere.ray_intersect(origin, direction, &distI);
         if (intersect.intersects) {
             max_sphere_distance = intersect.dist;
             const k = origin.add(direction.scalarMul(intersect.dist));
-            hit = k;
+            hit.* = k;
 
             const n = k.sub(sphere.center).normalize();
-            N = n;
+            N.* = n;
             mat.diffuseColor = sphere.mat.diffuseColor;
             mat.abledo = sphere.mat.abledo;
             mat.specularExponent = sphere.mat.specularExponent;
         }
     }
 
-    var checkerboard_distance = std.math.floatMax(f64);
+    var checkerboard_distance: f64 = std.math.floatMax(f64);
 
     if (@abs(direction.y) > 1E-3) {
         const d = -(origin.y + 4) / direction.y;
         const pt = origin.add(direction.scalarMul(d));
         if (d > 0.0 and @abs(pt.x) < 10 and pt.z < -10 and pt.z > -30 and d < checkerboard_distance) {
             checkerboard_distance = d;
-            hit = pt;
-            N = Vec3{ .x = 0, .y = 1, .z = 0 };
+            hit.* = pt;
+            N.* = Vec3{ .x = 0, .y = 1, .z = 0 };
             if (@mod(@as(i32, @intFromFloat(0.5 * hit.x + 1000)) + @as(i32, @intFromFloat(0.5 * hit.z)), 2) == 1) {
                 mat.diffuseColor = Vec3{ .x = 0.3, .y = 0.3, .z = 0.3 };
             } else {
@@ -390,7 +393,7 @@ fn sceneIntersect(origin: Vec3, direction: Vec3, hit: Vec3, N: Vec3, mat: Materi
         }
     }
 
-    return std.math.Min(max_sphere_distance, checkerboard_distance) < 1000.0;
+    return @min(max_sphere_distance, checkerboard_distance) < 1000.0;
 }
 
 // phong reflection
@@ -403,16 +406,16 @@ fn refract(I: Vec3, N: Vec3, etaT: f64, etaI: f64) Vec3 {
     const cos_i = @max(-1.0, @min(1.0, I.dot(N))) * -1.0;
     if (cos_i < 0.0) {
         // swap, the ray is inside the object
-        return refract(O, N.negate(), etaI, etaT);
+        return refract(I, N.negate(), etaI, etaT);
     }
 
     const eta = etaI / etaT;
 
-    const k = 1.0 - eta * eta * (1 - cosi * cosi);
+    const k = 1.0 - eta * eta * (1 - cos_i * cos_i);
 
     if (k < 0.0) {
         return Vec3{ .x = 1.0, .y = 0.0, .z = 0.0 };
     }
 
-    return I.scalarMul(eta).add(N.scalarMul((eta * cosi - std.math.sqrt(k))));
+    return I.scalarMul(eta).add(N.scalarMul((eta * cos_i - std.math.sqrt(k))));
 }
